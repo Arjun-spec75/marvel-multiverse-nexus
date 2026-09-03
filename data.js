53f2d17788733335b559f627c1189f845e78815d
Arjun-spec75 <a.l.arjun57@gmail.com>
2026-09-03 10:25:52 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,9 @@
+/**
+ * Marvel Multiverse Timeline - Legacy Entrypoint
+ * Bridges js/graphics.js and js/data.js for backwards compatibility.
+ */
+
+// If running in browser where js/graphics.js or js/data.js may be included
+if (typeof document !== 'undefined') {
+  // Ensured compatibility layer
+}
